274d5e93d2d7eada6d390bddcda5f24db3ae9882
caglargunay <caglarguenay@gmail.com>
2024-02-26 05:53:56 +0100

    11.02.24

--- src/test/java/cydeo/runners/CukesRunner.java
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "cydeo/steps",
         dryRun = false,
-        tags="@g",
+        tags="@smoke",
         publish = true
 )
 public class CukesRunner {}
